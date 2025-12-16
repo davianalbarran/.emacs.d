b8d1282158e7b45a4ad090d04317c55c20b11e59
Davian Albarran <dalbarran.dev@gmail.com>
2025-12-16 18:11:35 -0500

    init commit of base emacs config

---
+++ init.el
@@ -0,0 +1,47 @@
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(let ((default-directory  "~/.emacs.d/lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(require 'color-theme)
+(color-theme-initialize)
+(setq color-theme-is-global t)
+
+(load-theme `modus-vivendi-tinted t)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+(add-to-list 'exec-path' "~/.nvm/versions/node/v24.12.0/bin/")
+
+(setq package-selected-packages '(lsp-mode lsp-ui yasnippet lsp-treemacs helm-lsp
+    projectile hydra flycheck company avy which-key helm-xref dap-mode))
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
+
+(use-package lsp-ui)
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+(helm-mode)
+(require 'helm-xref)
+(define-key global-map [remap find-file] #'helm-find-files)
+(define-key global-map [remap execute-extended-command] #'helm-M-x)
+(define-key global-map [remap switch-to-buffer] #'helm-mini)
+
+(which-key-mode)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
+(add-hook 'csharp-mode-hook 'lsp)
+
+(setq company-minimum-prefix-length 1
+      company-idle-delay 0.0)
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-cpptools)
+  (yas-global-mode)
+  (setq lsp-modeline-diagnostics-scope :workspace))
