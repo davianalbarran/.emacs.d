90553177ec3cb62d372ed13509a84b9a9fd8cd0d
Davian Albarran <dalbarran@mitre.org>
2025-12-18 15:55:17 -0500

    configured emacs for vue work

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
 (add-to-list 'exec-path' "~/.nvm/versions/node/v24.12.0/bin/")
 
 (setq package-selected-packages '(lsp-mode lsp-ui yasnippet lsp-treemacs helm-lsp
-    projectile hydra flycheck company avy which-key helm-xref dap-mode multiple-cursors))
+    projectile hydra flycheck company company-box avy which-key helm-xref dap-mode web-mode multiple-cursors))
 
 (when (cl-find-if-not #'package-installed-p package-selected-packages)
   (package-refresh-contents)
@@ -27,6 +27,8 @@
 
 (use-package company-box
   :hook (company-mode . company-box-mode))
+
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 
 (helm-mode)
 (require 'helm-xref)
@@ -38,9 +40,11 @@
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
 (add-hook 'csharp-mode-hook 'lsp)
+(add-hook 'before-save-hook #'lsp-format-buffer t t)
 
 (setq company-minimum-prefix-length 1
-      company-idle-delay 0.0)
+      company-idle-delay 0.0
+      )
 
 (with-eval-after-load 'lsp-mode
   (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
@@ -63,3 +67,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7f0f6f6f8ca1b451a1fbb5a27c3fb904b939ea26fbecbf17caa4b64d67ffeea1"
+     "d20b3f5c1adf9cf2e2ad40a0f6172b32d53faa0489478cf359d2bdf432b9d8a4"
+     default))
+ '(package-selected-packages
+   '(avy company company-box dap-mode flycheck helm-lsp helm-xref hydra
+	 lsp-mode lsp-treemacs lsp-ui multiple-cursors projectile
+	 web-mode which-key yasnippet)))
