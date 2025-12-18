793ca1edc611a01964b6fe377eae47d1744bcb56
Davian Albarran <dalbarran.dev@gmail.com>
2025-12-18 12:40:26 -0500

    added capture template for todo

--- init.el
+++ init.el
@@ -56,6 +56,10 @@
 
 (setq org-default-notes-file "~/Desktop/todo.org")
 
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+datetree org-default-notes-file)
+         "* TODO %?\n SCHEDULED %^T" :time-prompt t)))
+
 ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
