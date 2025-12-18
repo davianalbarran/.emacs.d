f55e52f85b4cdb9ca3a68b629d5921996b38d1e5
Davian Albarran <dalbarran.dev@gmail.com>
2025-12-17 21:03:48 -0500

    added theme

--- init.el
+++ init.el
@@ -6,7 +6,9 @@
 (color-theme-initialize)
 (setq color-theme-is-global t)
 
-(load-theme `modus-vivendi-tinted t)
+(setq custom-theme-directory "~/.emacs.d/lisp/themes")
+
+(load-theme 'cappuccino-noir t)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -48,4 +50,20 @@
 
 (setq org-log-done 'note)
 
+<<<<<<< Updated upstream
 
+=======
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7f0f6f6f8ca1b451a1fbb5a27c3fb904b939ea26fbecbf17caa4b64d67ffeea1"
+     default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+>>>>>>> Stashed changes
